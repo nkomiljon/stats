b90ab9c7626824f6a865cc81625a650fbd385c28
najibulloShapoatov <abduholiq007@gmail.com>
2020-09-23 21:53:39 +0500

    Initial version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/najibulloShapoatov/stats
+
+go 1.15
+
+require github.com/najibulloShapoatov/bank v0.0.0-20200923164850-50e106636c84
---
+++ pkg/stats/stats.go
@@ -0,0 +1,26 @@
+package stats
+
+import (
+	"github.com/najibulloShapoatov/bank/pkg/types"
+
+)
+
+//Avg func return average amount from slice Payment
+func Avg(payments []types.Payment) types.Money {
+	var sum types.Money
+	for _, v := range payments {
+		sum += v.Amount
+	}
+	return sum / types.Money(len(payments))
+}
+
+//TotalInCategory  returned total sum in one category
+func TotalInCategory(payments []types.Payment, category types.Category) (total types.Money) {
+
+	for _, v := range payments {
+		if category == v.Category {
+			total += v.Amount
+		}
+	}
+	return
+}
